e905d4447d48791978ef18f65b4202d72a9961f3
yusup.kejarid <yusup.kejarid@gmail.com>
2021-08-06 23:48:17 +0700

    add fitur register

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -1,12 +1,33 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Register from '../Register';
+import Login from '../Login';
+import Dashboard from '../Dashboard';
 
 
 //TODO: INDEX 
 function App() {
   return (
-    <div className="container">
-      <h1>Hello world</h1>
-    </div>
+    <div>
+      <Router>
+        <div>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/">
+            <Dashboard />
+          </Route>
+        </div>
+      </Router>
+      </div>
   );
 }
 
---
+++ src/containers/pages/Register/index.js
@@ -0,0 +1,50 @@
+import React, { Component } from "react";
+import firebase from "../../../config/firebase";
+
+export default class Register extends Component {
+    state = {
+        email: '',
+        password: '',
+    }
+
+    handleChangeText = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value 
+        })
+    }
+    handleRegisterSubmit = () => {
+        const { email, password } = this.state;
+        console.log('email =', email, 'password =', password);
+
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+            .then(res => {
+                console.log('success', res);
+            })
+            .catch(function(error){
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log(errorCode, errorMessage);
+            });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="container mt-4">
+                    <h1>Register Page</h1>
+                    <div classname="input-group mb-3">
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label" >Email</label>
+                            <input type="email" className="form-control" id="email" placeholder="name@example.com" onChange={this.handleChangeText}/>
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="exampleFormControlInput1" className="form-label" >Password</label>
+                            <input type="password" className="form-control" id="password" placeholder="password" onChange={this.handleChangeText}/>
+                        </div>
+                        <button type="button" class="btn btn-primary" onClick={this.handleRegisterSubmit}>Register</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
