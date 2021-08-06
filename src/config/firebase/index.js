3a1aaecfc90a2038a82a3ca4cd39e505373de4cd
yusup.kejarid <yusup.kejarid@gmail.com>
2021-08-06 22:41:49 +0700

    configurasi firebase

---
+++ src/config/firebase/index.js
@@ -0,0 +1,16 @@
+import firebase from "firebase/app";
+import 'firebase/auth';
+import 'firebase/firestore';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAAr-cWSCn757xiLxxEA0xN75_KpXraNls",
+    authDomain: "react-firebase-a20e4.firebaseapp.com",
+    projectId: "react-firebase-a20e4",
+    storageBucket: "react-firebase-a20e4.appspot.com",
+    messagingSenderId: "1037708832707",
+    appId: "1:1037708832707:web:2f1cb755b7c5bd10109776"
+  };
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
+export default firebase;
